db771303b73f30419fd11c39360dd9c2b339dc09
Surya Suresh <ssuresh3@ucsc.edu>
2021-04-06 21:23:14 -0700

    pushing formatting changes

--- project1-1683357/codebase/rbf/pfm.cc
+++ project1-1683357/codebase/rbf/pfm.cc
@@ -1,88 +1,61 @@
 #include "pfm.h"
 
-PagedFileManager* PagedFileManager::_pf_manager = 0;
+PagedFileManager *PagedFileManager::_pf_manager = 0;
 
-PagedFileManager* PagedFileManager::instance()
-{
-    if(!_pf_manager)
+PagedFileManager *PagedFileManager::instance() {
+    if (!_pf_manager)
         _pf_manager = new PagedFileManager();
 
     return _pf_manager;
 }
 
-
-PagedFileManager::PagedFileManager()
-{
+PagedFileManager::PagedFileManager() {
 }
 
-
-PagedFileManager::~PagedFileManager()
-{
+PagedFileManager::~PagedFileManager() {
 }
 
-
-RC PagedFileManager::createFile(const string &fileName)
-{
+RC PagedFileManager::createFile(const string &fileName) {
     return -1;
 }
 
-
-RC PagedFileManager::destroyFile(const string &fileName)
-{
+RC PagedFileManager::destroyFile(const string &fileName) {
     return -1;
 }
 
-
-RC PagedFileManager::openFile(const string &fileName, FileHandle &fileHandle)
-{
+RC PagedFileManager::openFile(const string &fileName, FileHandle &fileHandle) {
     return -1;
 }
 
-
-RC PagedFileManager::closeFile(FileHandle &fileHandle)
-{
+RC PagedFileManager::closeFile(FileHandle &fileHandle) {
     return -1;
 }
 
-
-FileHandle::FileHandle()
-{
-	readPageCounter = 0;
-	writePageCounter = 0;
-	appendPageCounter = 0;
+FileHandle::FileHandle() {
+    readPageCounter = 0;
+    writePageCounter = 0;
+    appendPageCounter = 0;
 }
 
-
-FileHandle::~FileHandle()
-{
+FileHandle::~FileHandle() {
 }
 
-
-RC FileHandle::readPage(PageNum pageNum, void *data)
-{
+RC FileHandle::readPage(PageNum pageNum, void *data) {
     return -1;
 }
 
-
-RC FileHandle::writePage(PageNum pageNum, const void *data)
-{
+RC FileHandle::writePage(PageNum pageNum, const void *data) {
     return -1;
 }
 
-
-RC FileHandle::appendPage(const void *data)
-{
+RC FileHandle::appendPage(const void *data) {
     return -1;
 }
 
-
-unsigned FileHandle::getNumberOfPages()
-{
+unsigned FileHandle::getNumberOfPages() {
     return -1;
 }
 
-
-RC FileHandle::collectCounterValues(unsigned &readPageCount, unsigned &writePageCount, unsigned &appendPageCount)
-{
-	return -1;
+RC FileHandle::collectCounterValues(unsigned &readPageCount, unsigned &writePageCount, unsigned &appendPageCount) {
+    return -1;
 }
