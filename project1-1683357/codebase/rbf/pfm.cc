15e10dfdc7a2ae432cc147f44253faf8dee911ec
Surya Suresh <ssuresh3@unix2.lt.ucsc.edu>
2021-04-07 23:58:40 -0700

    implemented functions

--- project1-1683357/codebase/rbf/pfm.cc
+++ project1-1683357/codebase/rbf/pfm.cc
@@ -16,19 +16,75 @@ PagedFileManager::~PagedFileManager() {
 }
 
 RC PagedFileManager::createFile(const string &fileName) {
-    return -1;
+    int exists = access(fileName.c_str(), F_OK);
+
+    if(exists == 0) {
+        cout << "File " << fileName << " exists" << endl;
+        return -1;
+    }
+
+    FILE* fileHandle = fopen(fileName.c_str(), "w");
+    int res = fclose(fileHandle);
+
+    if(res != 0) {
+        cout << "File " << fileName << "didn't close properly" << endl;
+        return -1;
+    }
+
+    return 0;
 }
 
 RC PagedFileManager::destroyFile(const string &fileName) {
-    return -1;
+    int exists = access(fileName.c_str(), F_OK);
+
+    if(exists != 0) {
+        cout << "File " << fileName << "doesn't exist" << endl;
+        return -1;
+    }
+
+    int res = remove(fileName.c_str());
+
+    if(res != 0) {
+        cout << "File "<< fileName << " didn't delete properly" << endl;
+        return -1;
+    }
+
+    return 0;
 }
 
 RC PagedFileManager::openFile(const string &fileName, FileHandle &fileHandle) {
-    return -1;
+    int exists = access(fileName.c_str(), F_OK);
+
+    if(exists != 0) {
+        cout << "File " << fileName << "doesn't exist" << endl;
+        return -1;
+    }
+
+    if(fileHandle.getfd() != NULL) {
+        cout << "Filehandle for file " << fileName << " already exists" << endl;
+        return -1;
+    }
+
+    FILE* fd = fopen(fileName.c_str(), "r+");
+    fileHandle.setfd(fd);
+
+    return 0;
 }
 
 RC PagedFileManager::closeFile(FileHandle &fileHandle) {
-    return -1;
+    if(fileHandle.getfd() == NULL) {
+        cout << "Filehandle doesn't exist" << endl;
+        return -1;
+    }
+
+    int res = fclose(fileHandle.getfd());
+
+    if(res != 0) {
+        cout << "File didn't close properly" << endl;
+        return -1;
+    }
+
+    return 0;
 }
 
 FileHandle::FileHandle() {
@@ -58,4 +114,12 @@ unsigned FileHandle::getNumberOfPages() {
 
 RC FileHandle::collectCounterValues(unsigned &readPageCount, unsigned &writePageCount, unsigned &appendPageCount) {
     return -1;
+}
+
+void FileHandle::setfd(FILE* fd_) {
+    this->fd = fd_;
+}
+
+FILE* FileHandle::getfd() {
+    return this->fd;
 }
