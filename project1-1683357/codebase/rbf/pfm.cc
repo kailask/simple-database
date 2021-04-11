7f2385fca842e04fcb472f3885bab07db134dc99
Kailas Krivanka <38896831+kailask@users.noreply.github.com>
2021-04-10 18:51:33 -0700

    file handle?

--- project1-1683357/codebase/rbf/pfm.cc
+++ project1-1683357/codebase/rbf/pfm.cc
@@ -18,15 +18,15 @@ PagedFileManager::~PagedFileManager() {
 RC PagedFileManager::createFile(const string &fileName) {
     int exists = access(fileName.c_str(), F_OK);
 
-    if(exists == 0) {
+    if (exists == 0) {
         cout << "File " << fileName << " exists" << endl;
         return -1;
     }
 
-    FILE* fileHandle = fopen(fileName.c_str(), "w");
+    FILE *fileHandle = fopen(fileName.c_str(), "w");
     int res = fclose(fileHandle);
 
-    if(res != 0) {
+    if (res != 0) {
         cout << "File " << fileName << "didn't close properly" << endl;
         return -1;
     }
@@ -37,15 +37,15 @@ RC PagedFileManager::createFile(const string &fileName) {
 RC PagedFileManager::destroyFile(const string &fileName) {
     int exists = access(fileName.c_str(), F_OK);
 
-    if(exists != 0) {
+    if (exists != 0) {
         cout << "File " << fileName << "doesn't exist" << endl;
         return -1;
     }
 
     int res = remove(fileName.c_str());
 
-    if(res != 0) {
-        cout << "File "<< fileName << " didn't delete properly" << endl;
+    if (res != 0) {
+        cout << "File " << fileName << " didn't delete properly" << endl;
         return -1;
     }
 
@@ -55,31 +55,31 @@ RC PagedFileManager::destroyFile(const string &fileName) {
 RC PagedFileManager::openFile(const string &fileName, FileHandle &fileHandle) {
     int exists = access(fileName.c_str(), F_OK);
 
-    if(exists != 0) {
+    if (exists != 0) {
         cout << "File " << fileName << "doesn't exist" << endl;
         return -1;
     }
 
-    if(fileHandle.getfd() != NULL) {
+    if (fileHandle.getfd() != NULL) {
         cout << "Filehandle for file " << fileName << " already exists" << endl;
         return -1;
     }
 
-    FILE* fd = fopen(fileName.c_str(), "r+");
+    FILE *fd = fopen(fileName.c_str(), "r+");
     fileHandle.setfd(fd);
 
     return 0;
 }
 
 RC PagedFileManager::closeFile(FileHandle &fileHandle) {
-    if(fileHandle.getfd() == NULL) {
+    if (fileHandle.getfd() == NULL) {
         cout << "Filehandle doesn't exist" << endl;
         return -1;
     }
 
     int res = fclose(fileHandle.getfd());
 
-    if(res != 0) {
+    if (res != 0) {
         cout << "File didn't close properly" << endl;
         return -1;
     }
@@ -97,29 +97,62 @@ FileHandle::~FileHandle() {
 }
 
 RC FileHandle::readPage(PageNum pageNum, void *data) {
-    return -1;
+    int file_no = fileno(fd);
+    if (lseek(file_no, pageNum * PAGE_SIZE, SEEK_SET) == -1) {
+        return -1;
+    }
+
+    if (read(file_no, data, PAGE_SIZE) != PAGE_SIZE) {
+        return -1;
+    }
+
+    readPageCounter++;
+    return 0;
 }
 
 RC FileHandle::writePage(PageNum pageNum, const void *data) {
-    return -1;
+    int file_no = fileno(fd);
+    if (lseek(file_no, pageNum * PAGE_SIZE, SEEK_SET) == -1) {
+        return -1;
+    }
+
+    if (write(file_no, data, PAGE_SIZE) != PAGE_SIZE) {
+        return -1;
+    }
+
+    writePageCounter++;
+    return 0;
 }
 
 RC FileHandle::appendPage(const void *data) {
-    return -1;
+    int file_no = fileno(fd);
+    if (lseek(file_no, 0, SEEK_END) == -1) {
+        return -1;
+    }
+
+    if (write(file_no, data, PAGE_SIZE) != PAGE_SIZE) {
+        return -1;
+    }
+
+    appendPageCounter++;
+    return 0;
 }
 
 unsigned FileHandle::getNumberOfPages() {
-    return -1;
+    return appendPageCounter;
 }
 
 RC FileHandle::collectCounterValues(unsigned &readPageCount, unsigned &writePageCount, unsigned &appendPageCount) {
-    return -1;
+    readPageCount = readPageCounter;
+    writePageCount = writePageCounter;
+    appendPageCount = appendPageCounter;
+    return 0;
 }
 
-void FileHandle::setfd(FILE* fd_) {
+void FileHandle::setfd(FILE *fd_) {
     this->fd = fd_;
 }
 
-FILE* FileHandle::getfd() {
+FILE *FileHandle::getfd() {
     return this->fd;
 }
