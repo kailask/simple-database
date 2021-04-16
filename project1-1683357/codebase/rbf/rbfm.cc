88cbbad28b81a6db18ee5ef4dbb281d24d3998d2
Kailas Krivanka <38896831+kailask@users.noreply.github.com>
2021-04-15 19:08:52 -0700

    Simple functions

--- project1-1683357/codebase/rbf/rbfm.cc
+++ project1-1683357/codebase/rbf/rbfm.cc
@@ -1,37 +1,34 @@
 #include "rbfm.h"
 
-RecordBasedFileManager* RecordBasedFileManager::_rbf_manager = 0;
+RecordBasedFileManager *RecordBasedFileManager::_rbf_manager = 0;
 
-RecordBasedFileManager* RecordBasedFileManager::instance()
-{
-    if(!_rbf_manager)
+RecordBasedFileManager *RecordBasedFileManager::instance() {
+    if (!_rbf_manager)
         _rbf_manager = new RecordBasedFileManager();
 
     return _rbf_manager;
 }
 
-RecordBasedFileManager::RecordBasedFileManager()
-{
+RecordBasedFileManager::RecordBasedFileManager() {
 }
 
-RecordBasedFileManager::~RecordBasedFileManager()
-{
+RecordBasedFileManager::~RecordBasedFileManager() {
 }
 
 RC RecordBasedFileManager::createFile(const string &fileName) {
-    return -1;
+    return PagedFileManager::instance()->createFile(fileName);
 }
 
 RC RecordBasedFileManager::destroyFile(const string &fileName) {
-    return -1;
+    return PagedFileManager::instance()->destroyFile(fileName);
 }
 
 RC RecordBasedFileManager::openFile(const string &fileName, FileHandle &fileHandle) {
-    return -1;
+    return PagedFileManager::instance()->openFile(fileName, fileHandle);
 }
 
 RC RecordBasedFileManager::closeFile(FileHandle &fileHandle) {
-    return -1;
+    return PagedFileManager::instance()->closeFile(fileHandle);
 }
 
 RC RecordBasedFileManager::insertRecord(FileHandle &fileHandle, const vector<Attribute> &recordDescriptor, const void *data, RID &rid) {
