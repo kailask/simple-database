21656f0d92422477a05fb1d5c36383ad4c4e3c3d
Kailas Krivanka <38896831+kailask@users.noreply.github.com>
2021-05-03 19:05:33 -0700

    Work in progress

--- project2-01/codebase/rm/rm.cc
+++ project2-01/codebase/rm/rm.cc
@@ -1,88 +1,156 @@
 
 #include "rm.h"
 
-RelationManager* RelationManager::_rm = 0;
+RelationManager *RelationManager::_rm = 0;
 
-RelationManager* RelationManager::instance()
-{
-    if(!_rm)
+RelationManager *RelationManager::instance() {
+    if (!_rm)
         _rm = new RelationManager();
 
     return _rm;
 }
 
-RelationManager::RelationManager()
-{
+RelationManager::RelationManager() {
+    _cat = Catalog::openCatalog();
 }
 
-RelationManager::~RelationManager()
-{
+RelationManager::~RelationManager() {
+    delete _cat;
 }
 
-RC RelationManager::createCatalog()
-{
+RC RelationManager::createCatalog() {
+    rbfm->createFile("Tables.tbl");
+    FileHandle fd;
+    rbfm->openFile("Table.tbl", fd);
     return -1;
 }
 
-RC RelationManager::deleteCatalog()
-{
+RC RelationManager::deleteCatalog() {
     return -1;
 }
 
-RC RelationManager::createTable(const string &tableName, const vector<Attribute> &attrs)
-{
+RC RelationManager::createTable(const string &tableName, const vector<Attribute> &attrs) {
     return -1;
 }
 
-RC RelationManager::deleteTable(const string &tableName)
-{
+RC RelationManager::deleteTable(const string &tableName) {
     return -1;
 }
 
-RC RelationManager::getAttributes(const string &tableName, vector<Attribute> &attrs)
-{
+RC RelationManager::getAttributes(const string &tableName, vector<Attribute> &attrs) {
     return -1;
 }
 
-RC RelationManager::insertTuple(const string &tableName, const void *data, RID &rid)
-{
+RC RelationManager::insertTuple(const string &tableName, const void *data, RID &rid) {
     return -1;
 }
 
-RC RelationManager::deleteTuple(const string &tableName, const RID &rid)
-{
+RC RelationManager::deleteTuple(const string &tableName, const RID &rid) {
     return -1;
 }
 
-RC RelationManager::updateTuple(const string &tableName, const void *data, const RID &rid)
-{
+RC RelationManager::updateTuple(const string &tableName, const void *data, const RID &rid) {
     return -1;
 }
 
-RC RelationManager::readTuple(const string &tableName, const RID &rid, void *data)
-{
+RC RelationManager::readTuple(const string &tableName, const RID &rid, void *data) {
     return -1;
 }
 
-RC RelationManager::printTuple(const vector<Attribute> &attrs, const void *data)
-{
-	return -1;
+RC RelationManager::printTuple(const vector<Attribute> &attrs, const void *data) {
+    return -1;
 }
 
-RC RelationManager::readAttribute(const string &tableName, const RID &rid, const string &attributeName, void *data)
-{
+RC RelationManager::readAttribute(const string &tableName, const RID &rid, const string &attributeName, void *data) {
     return -1;
 }
 
 RC RelationManager::scan(const string &tableName,
-      const string &conditionAttribute,
-      const CompOp compOp,                  
-      const void *value,                    
-      const vector<string> &attributeNames,
-      RM_ScanIterator &rm_ScanIterator)
-{
+                         const string &conditionAttribute,
+                         const CompOp compOp,
+                         const void *value,
+                         const vector<string> &attributeNames,
+                         RM_ScanIterator &rm_ScanIterator) {
     return -1;
 }
 
+RelationManager::Catalog *RelationManager::Catalog::createCatalog() {
+    if (rbfm->createFile(meta_table_name) != SUCCESS ||
+        rbfm->createFile(columns_table_name) != SUCCESS) {
+        return NULL;
+    }
 
+    FileHandle meta_file;
+    FileHandle columns_file;
+    if (rbfm->openFile(meta_table_name, meta_file) != SUCCESS ||
+        rbfm->openFile(columns_table_name, columns_file) != SUCCESS) {
+        return NULL;
+    }
 
+    writeTable(meta_file, columns_file, meta_table_attrs, 0, meta_table_name);
+    writeTable(meta_file, columns_file, columns_table_attrs, 1, columns_table_name);
+
+    return new Catalog(meta_file, columns_file);
+}
+
+RelationManager::Catalog *RelationManager::Catalog::openCatalog() {
+    FileHandle meta_file;
+    FileHandle columns_file;
+    if (rbfm->openFile(meta_table_name, meta_file) != SUCCESS ||
+        rbfm->openFile(columns_table_name, columns_file) != SUCCESS) {
+        return NULL;
+    }
+
+    return new Catalog(meta_file, columns_file);
+}
+
+RelationManager::Catalog::Catalog(FileHandle meta_file, FileHandle columns_file) {
+    RBFM_ScanIterator it;
+
+    RID rid;
+    char data[PAGE_SIZE];
+    rbfm->scan(meta_file, meta_table_attrs, "", NO_OP, NULL, {"table-name"}, it);
+    while (it.getNextRecord(rid, data) != RBFM_EOF) {
+    }
+}
+
+RelationManager::Catalog::~Catalog() {
+    //free tables
+}
+
+RC RelationManager::Catalog::writeTable(FileHandle &meta_file, FileHandle &columns_file,
+                                        const vector<Attribute> &attrs, unsigned table_id, const string &table_name) {
+    char data[PAGE_SIZE];
+    RID rid;
+
+    prepareTuple(data, meta_table_attrs, {(void *)&table_id, (void *)table_name.c_str(), (void *)table_name.c_str()});
+    RC ret = rbfm->insertRecord(meta_file, meta_table_attrs, data, rid);
+    if (ret != SUCCESS) return ERROR;
+
+    for (size_t i = 0; i < attrs.size(); i++) {
+        Attribute attr = attrs[i];
+        prepareTuple(data, columns_table_attrs, {(void *)&table_id, (void *)attr.name.c_str(), (void *)&attr.type, &i, (void *)table_name.c_str()});
+        ret = rbfm->insertRecord(columns_file, columns_table_attrs, data, rid);
+        if (ret != SUCCESS) return ERROR;
+    }
+}
+
+static void
+prepareTuple(char *data, const vector<Attribute> &attrs, vector<void *> values) {
+    data += static_cast<int>(ceil((double)attrs.size() / CHAR_BIT));
+
+    for (size_t i = 0; i < attrs.size(); i++) {
+        switch (attrs[i].type) {
+            case TypeInt:
+            case TypeReal:
+                memcpy(data, values[i], 4);
+                data += 4;
+                break;
+            case TypeVarChar:
+                memcpy(data, &attrs[i].length, 4);
+                data += 4;
+                memcpy(data, values[i], strlen((char *)values[i]));
+                data += attrs[i].length;
+        }
+    }
+}
--- project2-01/codebase/rm/rm.h
+++ project2-01/codebase/rm/rm.h
@@ -2,73 +2,119 @@
 #ifndef _rm_h_
 #define _rm_h_
 
+#include <memory>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 #include "../rbf/rbfm.h"
 
+#define SUCCESS 0
+#define ERROR -1
+
 using namespace std;
 
-# define RM_EOF (-1)  // end of a scan operator
+#define RM_EOF (-1)  // end of a scan operator
+
+//System tables
+const static string meta_table_name = "tables";
+const static string columns_table_name = "columns";
+const static vector<Attribute> meta_table_attrs = {
+    {"table-id", TypeInt, 4},
+    {"table-name", TypeVarChar, 50},
+    {"file-name", TypeVarChar, 50}};
+const static vector<Attribute> columns_table_attrs = {
+    {"table-id", TypeInt, 4},
+    {"column-name", TypeVarChar, 50},
+    {"column-type", TypeInt, 4},
+    {"column-position", TypeInt, 4},
+    {"table-name", TypeVarChar, 50}};
+
+static RecordBasedFileManager *rbfm = RecordBasedFileManager::instance();
 
 // RM_ScanIterator is an iteratr to go through tuples
 class RM_ScanIterator {
-public:
-  RM_ScanIterator() {};
-  ~RM_ScanIterator() {};
+   public:
+    RM_ScanIterator(){};
+    ~RM_ScanIterator(){};
 
-  // "data" follows the same format as RelationManager::insertTuple()
-  RC getNextTuple(RID &rid, void *data) { return RM_EOF; };
-  RC close() { return -1; };
+    // "data" follows the same format as RelationManager::insertTuple()
+    RC getNextTuple(RID &rid, void *data) { return RM_EOF; };
+    RC close() { return -1; };
 };
 
-
 // Relation Manager
-class RelationManager
-{
-public:
-  static RelationManager* instance();
+class RelationManager {
+    struct Table {
+        vector<Attribute> attrs;
+        FileHandle file;
 
-  RC createCatalog();
+        Table(FileHandle f, vector<Attribute> a) : file(f), attrs(a){};
+        Table(vector<Attribute> a) : attrs(a){};
+        ~Table() { rbfm->closeFile(file); }
+    };
 
-  RC deleteCatalog();
+    class Catalog {
+       public:
+        unordered_map<string, Table> tables;
 
-  RC createTable(const string &tableName, const vector<Attribute> &attrs);
+        static Catalog *openCatalog();
+        static Catalog *createCatalog();
+        static RC writeTable(FileHandle &meta_file, FileHandle &columns_file,
+                             const vector<Attribute> &attrs, unsigned table_id, const string &table_name);
+        ~Catalog();
 
-  RC deleteTable(const string &tableName);
+       private:
+        Catalog(FileHandle meta_file, FileHandle columns_file);
+        unsigned newTableId() { return ++table_id; }
+        unsigned table_id = 0;
+    };
 
-  RC getAttributes(const string &tableName, vector<Attribute> &attrs);
+   public:
+    static RelationManager *instance();
 
-  RC insertTuple(const string &tableName, const void *data, RID &rid);
+    RC createCatalog();
 
-  RC deleteTuple(const string &tableName, const RID &rid);
+    RC deleteCatalog();
 
-  RC updateTuple(const string &tableName, const void *data, const RID &rid);
+    RC createTable(const string &tableName, const vector<Attribute> &attrs);
 
-  RC readTuple(const string &tableName, const RID &rid, void *data);
+    RC deleteTable(const string &tableName);
 
-  // Print a tuple that is passed to this utility method.
-  // The format is the same as printRecord().
-  RC printTuple(const vector<Attribute> &attrs, const void *data);
+    RC getAttributes(const string &tableName, vector<Attribute> &attrs);
 
-  RC readAttribute(const string &tableName, const RID &rid, const string &attributeName, void *data);
+    RC insertTuple(const string &tableName, const void *data, RID &rid);
 
-  // Scan returns an iterator to allow the caller to go through the results one by one.
-  // Do not store entire results in the scan iterator.
-  RC scan(const string &tableName,
-      const string &conditionAttribute,
-      const CompOp compOp,                  // comparison type such as "<" and "="
-      const void *value,                    // used in the comparison
-      const vector<string> &attributeNames, // a list of projected attributes
-      RM_ScanIterator &rm_ScanIterator);
+    RC deleteTuple(const string &tableName, const RID &rid);
 
+    RC updateTuple(const string &tableName, const void *data, const RID &rid);
 
-protected:
-  RelationManager();
-  ~RelationManager();
+    RC readTuple(const string &tableName, const RID &rid, void *data);
 
-private:
-  static RelationManager *_rm;
+    // Print a tuple that is passed to this utility method.
+    // The format is the same as printRecord().
+    RC printTuple(const vector<Attribute> &attrs, const void *data);
+
+    RC readAttribute(const string &tableName, const RID &rid, const string &attributeName, void *data);
+
+    // Scan returns an iterator to allow the caller to go through the results one by one.
+    // Do not store entire results in the scan iterator.
+    RC scan(const string &tableName,
+            const string &conditionAttribute,
+            const CompOp compOp,                   // comparison type such as "<" and "="
+            const void *value,                     // used in the comparison
+            const vector<string> &attributeNames,  // a list of projected attributes
+            RM_ScanIterator &rm_ScanIterator);
+
+   protected:
+    RelationManager();
+    ~RelationManager();
+
+   private:
+    static RelationManager *_rm;
+    Catalog *_cat;
+
+    static void prepareTuple(char *data, const vector<Attribute> &attrs, vector<void *> values);
 };
 
 #endif
