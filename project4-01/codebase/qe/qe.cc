624768c6a97bd875b5b1e7ec4a764aeb88a7a805
Kailas Krivanka <38896831+kailask@users.noreply.github.com>
2021-05-27 18:25:44 -0700

    formatting

--- project4-01/codebase/qe/qe.cc
+++ project4-01/codebase/qe/qe.cc
@@ -1,7 +1,7 @@
 
 #include "qe.h"
 
-Filter::Filter(Iterator* input, const Condition &condition) {
+Filter::Filter(Iterator* input, const Condition& condition) {
 }
 
 // ... the rest of your implementations go here
